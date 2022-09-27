a8b3710a96e23d74b21e3b94d57155bdecbc1653
tinaxd <31384595+tinaxd@users.noreply.github.com>
2022-09-28 00:45:17 +0900

    renderer object

--- Scripts/TestMain.cs
+++ Scripts/TestMain.cs
@@ -12,45 +12,17 @@ namespace GPLCraft
 
         // Called when the node enters the scene tree for the first time.
         private WorldGen gen;
+        private WorldRenderer wRenderer;
 
         public override void _Ready()
         {
             // GD.PushError("OnREADY!!");
 
+            wRenderer = GetNode<WorldRenderer>("WorldOrigin");
 
             gen = new WorldGen(5);
             var chunk = gen.GenerateChunk(0, 0);
-            RenderChunk(chunk);
-        }
-
-        private void RenderChunk(Chunk c)
-        {
-            var dirts = new List<Vector3>();
-
-            for (int k = 0; k < Chunk.SizeY; k++)
-            {
-                for (int i = 0; i < Chunk.SizeX; i++)
-                {
-                    for (int j = 0; j < Chunk.SizeZ; j++)
-                    {
-                        Block b = c.GetBlock(i, k, j);
-                        if (b.BlockID != 0)
-                        {
-                            // is not air
-                            dirts.Add(new Vector3(i, k, j));
-                        }
-                    }
-                }
-            }
-
-            var multimesh = GetNode<MultiMeshInstance>("multimesh");
-            multimesh.Multimesh.InstanceCount = dirts.Count;
-            for (int i = 0; i < dirts.Count; i++)
-            {
-                var position = multimesh.Multimesh.GetInstanceTransform(i);
-                position = position.Translated(dirts[i]);
-                multimesh.Multimesh.SetInstanceTransform(i, position);
-            }
+            wRenderer.RenderChunk(chunk);
         }
 
         //  // Called every frame. 'delta' is the elapsed time since the previous frame.
---
+++ Scripts/WorldRenderer.cs
@@ -0,0 +1,50 @@
+using Godot;
+using System;
+using System.Collections.Generic;
+
+namespace GPLCraft
+{
+    public class WorldRenderer : Spatial
+    {
+        // Declare member variables here. Examples:
+        // private int a = 2;
+        // private string b = "text";
+
+        // Called when the node enters the scene tree for the first time.
+        public override void _Ready()
+        {
+
+        }
+
+        public void RenderChunk(Chunk c)
+        {
+            var dirts = new List<Vector3>();
+
+            for (int k = 0; k < Chunk.SizeY; k++)
+            {
+                for (int i = 0; i < Chunk.SizeX; i++)
+                {
+                    for (int j = 0; j < Chunk.SizeZ; j++)
+                    {
+                        Block b = c.GetBlock(i, k, j);
+                        if (b.BlockID != 0)
+                        {
+                            // is not air
+                            dirts.Add(new Vector3(i, k, j));
+                        }
+                    }
+                }
+            }
+
+            var multimesh = GetNode<MultiMeshInstance>("multimesh");
+            multimesh.Multimesh.InstanceCount = dirts.Count;
+            for (int i = 0; i < dirts.Count; i++)
+            {
+                var position = multimesh.Multimesh.GetInstanceTransform(i);
+                position = position.Translated(dirts[i]);
+                multimesh.Multimesh.SetInstanceTransform(i, position);
+            }
+        }
+    }
+
+}
