926db1125baf0666829bef6be2c1f2f0bf7a56cc
tinaxd <31384595+tinaxd@users.noreply.github.com>
2022-09-28 14:06:12 +0900

    c

--- src/ChunkRenderer.cpp
+++ src/ChunkRenderer.cpp
@@ -1,5 +1,8 @@
 #include "ChunkRenderer.h"
 #include <SurfaceTool.hpp>
+#include <ResourceLoader.hpp>
+#include <ArrayMesh.hpp>
+#include <Material.hpp>
 
 using namespace gplcraft;
 
@@ -31,15 +34,104 @@ void ChunkRenderer::render_chunk()
     if (!dirty_)
         return;
 
-    godot::Ref<godot::MeshInstance> mesh;
+    auto mesh = generate_mesh();
+
+    auto block_material = godot::ResourceLoader::get_singleton()->load("res://Materials/BlockMaterial.tres");
+
+    mesh_instance_->set_mesh(mesh);
+    mesh_instance_->set_material_override(block_material);
 
     dirty_ = false;
 }
 
 constexpr int ALL_FACES = XNEGF | XPOSF | YNEGF | YPOSF | ZNEGF | ZPOSF;
 
-static void add_face(godot::Ref < godot::SurfaceTool st, int faces, int x, int y, int z)
+static void _av(godot::Ref<godot::SurfaceTool> st, int x, int y, int z, int bx, int by, int bz, godot::Vector3 normal, int ux, int uy)
 {
+    float uxf = (16.0 / 256.0) * (2 + (1 - ux));
+    float uyf = (16.0 / 256.0) * (15 + (1 - uy));
+    st->add_uv(godot::Vector2(uxf, uyf));
+    st->add_normal(normal);
+    st->add_vertex(godot::Vector3(x, y, z));
+}
+
+static void add_face(godot::Ref<godot::SurfaceTool> s, int face, int x, int y, int z)
+{
+    switch (face)
+    {
+    case XPOSF:
+    {
+        auto norm = godot::Vector3(1, 0, 0);
+        _av(s, 1, 0, 1, x, y, z, norm, 0, 1);
+        _av(s, 1, 1, 1, x, y, z, norm, 1, 1);
+        _av(s, 1, 1, 0, x, y, z, norm, 1, 0);
+
+        _av(s, 1, 1, 0, x, y, z, norm, 1, 0);
+        _av(s, 1, 0, 0, x, y, z, norm, 0, 0);
+        _av(s, 1, 0, 1, x, y, z, norm, 0, 1);
+        break;
+    }
+    case YNEGF:
+    {
+        auto norm = godot::Vector3(0, 1, 0);
+        _av(s, 0, 0, 1, x, y, z, norm, 0, 1);
+        _av(s, 1, 0, 1, x, y, z, norm, 1, 1);
+        _av(s, 1, 0, 0, x, y, z, norm, 1, 0);
+
+        _av(s, 1, 0, 0, x, y, z, norm, 1, 0);
+        _av(s, 0, 0, 0, x, y, z, norm, 0, 0);
+        _av(s, 0, 0, 1, x, y, z, norm, 0, 1);
+        break;
+    }
+    case XNEGF:
+    {
+        auto norm = godot::Vector3(-1, 0, 0);
+        _av(s, 0, 1, 1, x, y, z, norm, 0, 1);
+        _av(s, 0, 0, 1, x, y, z, norm, 1, 1);
+        _av(s, 0, 0, 0, x, y, z, norm, 1, 0);
+
+        _av(s, 0, 0, 0, x, y, z, norm, 1, 0);
+        _av(s, 0, 1, 0, x, y, z, norm, 0, 0);
+        _av(s, 0, 1, 1, x, y, z, norm, 0, 1);
+        break;
+    }
+    case YPOSF:
+    {
+        auto norm = godot::Vector3(0, 1, 0);
+        _av(s, 1, 1, 1, x, y, z, norm, 0, 1);
+        _av(s, 0, 1, 1, x, y, z, norm, 1, 1);
+        _av(s, 0, 1, 0, x, y, z, norm, 1, 0);
+
+        _av(s, 0, 1, 0, x, y, z, norm, 1, 0);
+        _av(s, 1, 1, 0, x, y, z, norm, 0, 0);
+        _av(s, 1, 1, 1, x, y, z, norm, 0, 1);
+        break;
+    }
+    case ZPOSF:
+    {
+        auto norm = godot::Vector3(0, 0, 1);
+        _av(s, 0, 1, 1, x, y, z, norm, 0, 1);
+        _av(s, 1, 1, 1, x, y, z, norm, 1, 1);
+        _av(s, 1, 0, 1, x, y, z, norm, 1, 0);
+
+        _av(s, 1, 0, 1, x, y, z, norm, 1, 0);
+        _av(s, 0, 0, 1, x, y, z, norm, 0, 0);
+        _av(s, 0, 1, 1, x, y, z, norm, 0, 1);
+        break;
+    }
+    case ZNEGF:
+    {
+        auto norm = godot::Vector3(0, 0, -1);
+        _av(s, 0, 0, 0, x, y, z, norm, 0, 1);
+        _av(s, 1, 0, 0, x, y, z, norm, 1, 1);
+        _av(s, 1, 1, 0, x, y, z, norm, 1, 0);
+
+        _av(s, 1, 1, 0, x, y, z, norm, 1, 0);
+        _av(s, 0, 1, 0, x, y, z, norm, 0, 0);
+        _av(s, 0, 0, 0, x, y, z, norm, 0, 1);
+        break;
+    }
+    }
 }
 
 static void add_faces(godot::Ref<godot::SurfaceTool> st, int faces, int x, int y, int z)
@@ -89,9 +181,9 @@ godot::Ref<godot::Mesh> ChunkRenderer::generate_mesh()
                 args.append(j);
                 args.append(k);
                 auto result = chunk_->call("get_block_pos", args);
-                ERR_FAIL_COND(result.get_type() != result.OBJECT);
+                ERR_MSG_COND(result.get_type() != result.OBJECT);
                 godot::Object *block = result;
-                ERR_FAIL_COND(block == nullptr);
+                ERR_MSG_COND(block == nullptr);
 
                 int block_id = block->get("block_id");
                 if (block_id == 0)
@@ -128,4 +220,27 @@ godot::Ref<godot::Mesh> ChunkRenderer::generate_mesh()
             }
         }
     }
+
+    return st->commit();
+}
+
+bool ChunkRenderer::block_exists(int x, int y, int z)
+{
+    if (x < 0 || x >= 16 || y < 0 || y >= 128 || z < 0 || z >= 16)
+    {
+        return false;
+    }
+
+    godot::Array args;
+    args.append(x);
+    args.append(y);
+    args.append(z);
+    auto o = chunk_->call("get_block_pos", args);
+    ERR_MSG_COND(o.get_type() != o.OBJECT);
+
+    godot::Object *block = o;
+    ERR_MSG_COND(block == nullptr);
+
+    int block_id = block->get("block_id");
+    return block_id != 0;
 }
