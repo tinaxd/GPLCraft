d35264f04208ccbaca67545f0560fb67b25d57a7
tinaxd <31384595+tinaxd@users.noreply.github.com>
2022-09-28 13:41:48 +0900

    c;;

--- src/ChunkRenderer.cpp
+++ src/ChunkRenderer.cpp
@@ -1,4 +1,5 @@
 #include "ChunkRenderer.h"
+#include <SurfaceTool.hpp>
 
 using namespace gplcraft;
 
@@ -13,4 +14,118 @@ void ChunkRenderer::_init()
 
 void ChunkRenderer::_ready()
 {
+}
+
+ChunkRenderer::~ChunkRenderer()
+{
+    if (chunk_ != nullptr)
+    {
+        chunk_->unreference();
+    }
+}
+
+void ChunkRenderer::render_chunk()
+{
+    if (chunk_ == nullptr)
+        return;
+    if (!dirty_)
+        return;
+
+    godot::Ref<godot::MeshInstance> mesh;
+
+    dirty_ = false;
+}
+
+constexpr int ALL_FACES = XNEGF | XPOSF | YNEGF | YPOSF | ZNEGF | ZPOSF;
+
+static void add_face(godot::Ref < godot::SurfaceTool st, int faces, int x, int y, int z)
+{
+}
+
+static void add_faces(godot::Ref<godot::SurfaceTool> st, int faces, int x, int y, int z)
+{
+    auto f = [faces](int f)
+    { return faces & f != 0; };
+    if (f(XNEGF))
+    {
+        add_face(st, XNEGF, x, y, z);
+    }
+    if (f(XPOSF))
+    {
+        add_face(st, XPOSF, x, y, z);
+    }
+    if (f(YNEGF))
+    {
+        add_face(st, YNEGF, x, y, z);
+    }
+    if (f(YPOSF))
+    {
+        add_face(st, YPOSF, x, y, z);
+    }
+    if (f(ZNEGF))
+    {
+        add_face(st, ZNEGF, x, y, z);
+    }
+    if (f(ZPOSF))
+    {
+        add_face(st, ZPOSF, x, y, z);
+    }
+}
+
+godot::Ref<godot::Mesh> ChunkRenderer::generate_mesh()
+{
+    godot::Ref<godot::SurfaceTool> st;
+    st.instance();
+
+    st->begin(godot::Mesh::PRIMITIVE_TRIANGLES);
+    for (int i = 0; i < 16; i++)
+    {
+        for (int j = 0; j < 128; j++)
+        {
+            for (int k = 0; k < 16; k++)
+            {
+                godot::Array args;
+                args.append(i);
+                args.append(j);
+                args.append(k);
+                auto result = chunk_->call("get_block_pos", args);
+                ERR_FAIL_COND(result.get_type() != result.OBJECT);
+                godot::Object *block = result;
+                ERR_FAIL_COND(block == nullptr);
+
+                int block_id = block->get("block_id");
+                if (block_id == 0)
+                {
+                    // air block
+                    continue;
+                }
+
+                int faces = ALL_FACES;
+                if (block_exists(i - 1, j, k))
+                {
+                    faces &= ~XNEGF;
+                }
+                if (block_exists(i + 1, j, k))
+                {
+                    faces &= ~XPOSF;
+                }
+                if (block_exists(i, j - 1, k))
+                {
+                    faces &= ~YNEGF;
+                }
+                if (block_exists(i, j + 1, k))
+                {
+                    faces &= ~YPOSF;
+                }
+                if (block_exists(i, j, k - 1))
+                {
+                    faces &= ~ZNEGF;
+                }
+                if (block_exists(i, j, k + 1))
+                {
+                    faces &= ~ZPOSF;
+                }
+            }
+        }
+    }
 }
--- src/ChunkRenderer.h
+++ src/ChunkRenderer.h
@@ -4,9 +4,19 @@
 #include <Godot.hpp>
 #include <Spatial.hpp>
 #include <MeshInstance.hpp>
+#include <Mesh.hpp>
 
 namespace gplcraft
 {
+    enum
+    {
+        XNEGF = 1,
+        XPOSF = 2,
+        YNEGF = 4,
+        YPOSF = 8,
+        ZNEGF = 16,
+        ZPOSF = 32,
+    };
 
     class ChunkRenderer : public godot::Spatial
     {
@@ -19,12 +29,37 @@ namespace gplcraft
 
         void render_chunk();
 
+        godot::Ref<godot::Mesh> generate_mesh();
+
+        bool block_exists(int x, int y, int z);
+
     public:
+        ~ChunkRenderer();
+
         static void _register_methods();
 
         void _init();
 
         void _ready();
+
+        godot::Reference *get_chunk()
+        {
+            return chunk_;
+        }
+
+        void set_chunk(godot::Reference *chunk)
+        {
+            if (chunk_ != nullptr)
+            {
+                chunk_->unreference();
+            }
+            chunk->reference();
+            chunk_ = chunk;
+            dirty_ = true;
+            render_chunk();
+        }
+
+        void set_chunk_location(int cx, int cz);
     };
 
 }
